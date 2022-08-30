2b0f3a8a7c25d2d638dbbebb434ff3bc54650759
imahdimir <imahdimir@gmail.com>
2022-08-31 01:43:28 +0430

    v2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-##
-
 """
 
     """
 
-import pandas as pd
-from githubdata import GithubData
-from mirutil import funcs as mf
-from aiohttp import ClientSession
 import asyncio
-import nest_asyncio
+
+import pandas as pd
+from aiohttp import ClientSession
+from githubdata import GithubData
+from mirutil import async_requests as areq
+from mirutil.df_utils import read_data_according_to_type as read_data
+from mirutil.df_utils import save_as_prq_wo_index as sprq
+from mirutil import utils as mu
+from mirutil.jdate import make_zero_padded_jdate_ie_iso_fmt
 
 
-nest_asyncio.apply()
-
-stch_rp_url = 'https://github.com/imahdimir/d-Status-changes'
-cur_rp_url = 'https://github.com/imahdimir/get-Status-changes-TSETMC'
+targ_rp_url = 'https://github.com/imahdimir/d-firm-status-change'
+t2f_url = 'https://github.com/imahdimir/d-TSETMC_ID-2-FirmTicker'
 
 btic = 'BaseTicker'
 tic = 'Ticker'
@@ -24,22 +24,13 @@ url = 'url'
 nsta = 'NewStatus'
 time = 'Time'
 jdt = 'JDate'
+row = 'Row'
+jdtime = 'JDateTime'
 
 status_chng_burl = 'http://tsetmc.com/Loader.aspx?Partree=15131L&i='
 
-def status_change_url(tsetmc_id) :
+def make_status_change_url(tsetmc_id) :
   return f'{status_chng_burl}{tsetmc_id}'
-
-async def get_resp_text(url , trust_env = True) :
-  async with ClientSession(trust_env = trust_env) as session :
-    async with session.get(url) as resp :
-      return await resp.text()
-
-async def get_resps_of_urls(urls , trust_env = True) :
-  fu = get_resp_text
-  tasks = [asyncio.create_task(fu(x , trust_env = trust_env)) for x in urls]
-
-  return await asyncio.gather(*tasks)
 
 def build_df_for_each_id(id , resp_text) :
   dfs = pd.read_html(resp_text)
@@ -48,39 +39,42 @@ def build_df_for_each_id(id , resp_text) :
 
   df = dfs[0]
   df[tid] = id
+  df[row] = df.index
 
   return df
 
 def main() :
   pass
-
   ##
-  idf = pd.read_parquet('in.prq')
+  rp_t2f = GithubData(t2f_url)
+  rp_t2f.clone()
   ##
-  idf = idf.dropna()
+  dftfp = rp_t2f.data_fp
+  dft = read_data(dftfp)
   ##
-  idf[url] = idf[tid].apply(status_change_url)
+  dft = dft[[tid]]
+  dft.drop_duplicates(inplace = True)
+  dft.dropna(inplace = True)
   ##
-  cls = mf.return_clusters_indices(idf[url])
+  dft[url] = dft[tid].apply(make_status_change_url)
+  ##
+  cls = mu.return_clusters_indices(dft)
   ##
   for se in cls :
     print(se)
 
-    urls = idf.loc[se[0] : se[1] , url]
+    urls = dft.loc[se[0] : se[1] , url]
 
-    out = asyncio.run(get_resps_of_urls(urls))
-    idf.loc[se[0] : se[1] , 't'] = out
+    out = asyncio.run(areq.get_reps_texts_async(urls))
+    dft.loc[se[0] : se[1] , 'r'] = out
 
     # break
 
   ##
-  idf.to_parquet('temp0.prq' , index = False)
-
-  ##
   sdf = pd.DataFrame()
 
-  for _ , row in idf.iterrows() :
-    _df = build_df_for_each_id(row[tid] , row['t'])
+  for _ , _row in dft.iterrows() :
+    _df = build_df_for_each_id(_row[tid] , _row['r'])
     sdf = pd.concat([sdf , _df])
 
   ##
@@ -92,39 +86,45 @@ def main() :
 
   sdf = sdf.rename(columns = ren)
   ##
-  sdf = sdf[[tid , jdt , time , nsta]]
+  sdf[jdt] = sdf[jdt].apply(make_zero_padded_jdate_ie_iso_fmt)
   ##
-  sdf = sdf.drop_duplicates()
+  sdf[jdtime] = sdf[jdt] + ' ' + sdf[time]
   ##
-  sdf[jdt] = sdf[jdt].apply(mf.make_zero_padded_jdate)
+  sdf = sdf[[tid , row , jdtime , nsta]]
   ##
-  sdf.to_parquet('out.prq' , index = False)
+  sdf.drop_duplicates(inplace = True)
+  ##
+  rp_targ = GithubData(targ_rp_url)
+  rp_targ.clone()
+  ##
+  dffp = rp_targ.data_fp
+  ##
+  sprq(sdf , dffp)
+  ##
+  cur_url = 'https://github.com/imahdimir/b-' + rp_targ.repo_name
+  ##
+  tokfp = '/Users/mahdi/Dropbox/tok.txt'
+  tok = mu.get_tok_if_accessible(tokfp)
+  ##
+  msg = 'updated'
+  msg += ' by: ' + cur_url
 
-  ##
-  stch_rp = GithubData(stch_rp_url)
-  stch_rp.clone()
-
-  ##
-  pdfpn = stch_rp.data_filepath
-  pdf = pd.read_parquet(pdfpn)
-  ##
-  pdf = pd.concat([pdf , sdf])
-  ##
-  pdf = pdf.drop_duplicates()
-  ##
-  pdf.to_parquet(pdfpn , index = False)
-  ##
-  msg = 'init'
-  msg += ' by: ' + cur_rp_url
-
-  stch_rp.commit_push(msg)
-
-  ##
-
-  stch_rp.rmdir()
-
+  rp_targ.commit_and_push(msg , user = rp_targ.user_name , token = tok)
 
   ##
 
+  rp_targ.rmdir()
+  rp_t2f.rmdir()
+
+
+  ##
+
+##
+
+
+if __name__ == '__main__' :
+  main()
+
+##
 
 ##
